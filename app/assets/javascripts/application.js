6e899d91cf498e932561ca42095451c4e5d40727
SFraiz <sergi.fraiz12@gmail.com>
2016-12-11 12:31:49 +0100

    Add correct img size user index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$().ready(()=> {
+	console.log('hola');
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     
-  has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/Profile/missing.png"
+  has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "300x300>" }, default_url: "/assets/Profile/missing.png"
   validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\z/
   validates_presence_of :password, on: :create
   validates :nickname, uniqueness: true
